35b5fc496d7c2b1f0aacda161728aab4cf5e56d5
Nicholas Galante <nicholasgalante94@gmail.com>
2022-12-30 18:15:47 -0500

    Update fibonacci function

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,24 +1,36 @@
 function fibonacci(num) {
-  // type your code here
+  const fibArr = [0,1];
+  for (let i = 2; i < 30; i++) {
+    fibArr.push(fibArr[i-1] + fibArr[i-2])
+  }
+  return fib[num];
 }
 
+
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 0");
-  console.log("=>", fibonacci(0));
 
-  console.log("");
+  console.log("TEST 1 - Expecting: 0");
+  console.log("Result:", fibonacci(0));
 
-  console.log("Expecting: 1");
-  console.log("=>", fibonacci(2));
+  console.log("TEST 2 - Expecting: 1");
+  console.log("Result:", fibonacci(2));
 
-  console.log("");
-
-  console.log("Expecting: 55");
-  console.log("=>", fibonacci(10));
+  console.log("TEST 3 - Expecting: 55");
+  console.log("Result:", fibonacci(10));
 }
 
 module.exports = fibonacci;
 
-// Please add your pseudocode to this file
+/*  Pseudocode:
+create fibonacci array, starting with 0 and 1 as first two indexes
+  create empty array 
+  for loop, 30 iterations
+  for each index, push (index - 1) and (index - 2)
+return fibonacci array(num)
+*/
+
+
 // And a written explanation of your solution
+
+
+
